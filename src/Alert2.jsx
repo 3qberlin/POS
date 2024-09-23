5019224c6c66dba8a62e2af0eb81598022b4975b
Berlin <3q.berlin@gmail.com>
2024-09-23 17:20:09 +0800

    build customer alert2 button style

--- src/Alert2.jsx
+++ src/Alert2.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react';
 import SweetAlert2 from 'react-sweetalert2';
+import './css/Alert2.css';
 
-export default function Alert2(){
+export default function Alert2() {
     const [swalProps, setSwalProps] = useState({});
     return (
         <div>
-            <button onClick={() => {
+            <span className="material-symbols-outlined" onClick={() => {
                 setSwalProps({
                     show: true,
-                    title: 'Error!',
-                    text: 'Do you want to continue',
-                    icon: 'error',
-                    iconColor: 'blue',
-                    confirmButtonText: 'Cool',
-                    willClose: () => setSwalProps({}) 
+                    title: '確定登出？',
+                    confirmButtonText: '確定',
+                    showCancelButton: true,
+                    cancelButtonText: '取消',
+                    willClose: () => setSwalProps({}),
+                    reverseButtons:true,
+                    // cancelButtonColor:"gray",
+                    buttonsStyling: true
                 });
             }}>
-                Open
-            </button>
-
+                logout
+            </span>
+            
             <SweetAlert2 {...swalProps} />
         </div>
     );

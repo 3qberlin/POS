1588bd5e545542efce778b15afa40598220c1e85
Berlin <3q.berlin@gmail.com>
2024-10-23 11:05:53 +0800

    fix renderInput in MUI problem

--- src/DatePickerView.jsx
+++ src/DatePickerView.jsx
@@ -105,7 +105,7 @@ export default function DateComponent() {
                             setSelectedDate(date);
                         }}
                         shouldDisableDate={shouldDisableDate}
-                        renderInput={(params) => <TextField {...params} />}
+                        slotProps={{ textField: { fullWidth: true } }}
                     />
                 </div>
             </LocalizationProvider>
